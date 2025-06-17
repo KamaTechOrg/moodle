5c237792b7263277d4e2dd673a6667001a9120cf
Noa-gv <noagv1765@gmail.com>
2025-06-17 12:01:45 +0300

    Updating the lib file

--- mod/typinglesson/lib.php
+++ mod/typinglesson/lib.php
@@ -15,3 +15,9 @@ function typinglesson_before_standard_html_head() {
     global $PAGE;
     $PAGE->requires->css('/mod/typinglesson/css/icon.css');
 }
+
+function typinglesson_get_coursemodule_info($coursemodule) {
+    $info = new cached_cm_info();
+    $info->icon = new moodle_url('/mod/typinglesson/pix/icon.svg');
+    return $info;
+}
