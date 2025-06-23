fb8c0160b7bae28295a0afced1badd52f5e091d2
root <root@TamarLevi-PC.localdomain>
2025-06-23 16:54:46 +0300

    Add config.sample.php - demonstrates how to change Moodle theme

---
+++ config.sample.php
@@ -0,0 +1,17 @@
+<?php
+
+$CFG = new stdClass();
+
+$CFG->theme = 'boost';
+// $CFG->theme = 'classic';
+// $CFG->theme = 'adaptable';
+// $CFG->theme = 'mycustom';
+
+// Explanation for Mahat:
+// The file config.sample.php is a sample file demonstrating how to change the Moodle theme.
+// To activate a different theme, simply uncomment (//) the desired line.
+
+// Please note: This is a sample file only and does not contain any sensitive information such as database username or password.
+// The original config.php file was not uploaded for security reasons.
+
+
